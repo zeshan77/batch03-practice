199e15dfb083f8be32bf7c2cb205b8e25db9043b
Muhammad Zeshan <zeshan77@gmail.com>
2023-02-01 10:37:09 +0500

    migration, model and fetching data from database.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index']);
