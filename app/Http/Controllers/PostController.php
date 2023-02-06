769a9012150af8bce7ad8a4ad8002a3ee644d3da
Muhammad Zeshan <zeshan77@gmail.com>
2023-02-06 10:43:45 +0500

    Class 05: Eloquent relationships

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,19 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('title', 'LIKE', '%Enim%')
-            ->take(5)
-            ->orderBy('id', 'asc')
-            ->first();
+//        $posts = Post::where('title', 'LIKE', '%Enim%')
+//            ->take(5)
+//            ->orderBy('id', 'asc')
+//            ->first();
+//
+////        $posts = Post::first();
+//
+//        return $posts;
 
-//        $posts = Post::first();
 
-        return $posts;
+
+//        $user = User::with('posts')->first();
+//
+//        return $user;
+
+
+        $post = Post::with('user')
+            ->orderBy('id', 'desc')
+            ->get();
+
+        return $post;
+
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::first();
+
+        $post = $user->posts()->create([
+            'title' => $request->get('title'),
+            'description' => $request->get('description'),
+        ]);
+
+        return $post;
     }
 }
---
+++ database/migrations/2023_02_06_045135_add_user_id_column_to_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+
+            $table->foreign('user_id')
+                ->on('users')
+                ->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_user_id_foreign');
+
+            $table->dropColumn('user_id');
+
+        });
+    }
+};
